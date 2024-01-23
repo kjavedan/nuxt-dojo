1a8ee814cd1e26e39c8e8ee8890444cd57a2b2b4
Khaled Javedan <khaledjavedan76@gmail.com>
2024-01-23 12:54:47 +0400

    ready to deploy

--- server/api/ninja.js
+++ server/api/ninja.js
@@ -1,7 +1,7 @@
 export default defineEventHandler(async (event) => {
-  const { data } = await $fetch(
-    "https://api.currencyapi.com/v3/latest?apikey=cur_live_WcU5yHTfgaxwCahxylWMy2Z5yOuPfouRlobWibR3"
-  );
+  const { currencyKey } = useRuntimeConfig();
+
+  const { data } = await $fetch(currencyKey);
 
   return data;
 });
